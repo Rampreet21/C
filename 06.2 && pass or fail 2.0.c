f975df508c54d4026aa3bb5a8911641f5764cb58
Rampreet21 <rr6975871@gmail.com>
2024-08-16 22:52:09 +0530

    update new file

--- 06.2 && pass or fail 2.0.c
+++ 06.2 && pass or fail 2.0.c
@@ -10,10 +10,13 @@ int main ()
     scanf("%d", &marks2);
     printf("Math: ");
     scanf("%d", &marks3);
+
     float P=(marks1+marks2+marks3)/3;
+    
     if (marks1<=100 && marks2<=100 && marks3<=100){
         printf("Your Total Marks is %d.\n", T=marks1+marks2+marks3);
-        printf("You percentage is %.2f%%\n", P);         
+        printf("You percentage is %.2f%%\n", P);
+
         if (marks1<33 || marks2<33 || marks3<33){
         printf("You are failed due to less marks in individual subject.\n");
         }
