418310feb75882236333da8040a90ad54c2c3550
Rampreet21 <rr6975871@gmail.com>
2024-10-22 23:34:02 +0530

    update

---
+++ 13 arrey.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{                   // Integer Array
+    int a[5] = {10, 20, 30, 40, 50};
+
+    printf("%d %d %d %d %d \n", a[0], a[1], a[2], a[3], a[4]);
+
+                    // single character Array
+    char b[8] = {'R', 'a', 'm', 'p', 'r', 'e', 'e', 't'};
+
+    printf("%c%c%c%c%c%c%c%c", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]);
+
+    return 0;
+}
---
+++ 13.2 char 2D Array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{               // Character 2D Array
+    char a[2][9] = {"Rampreet", "Harpreet"};
+
+    printf("%s %s", a[0], a[1]);
+
+    return 0;
+}
