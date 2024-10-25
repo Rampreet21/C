933ea04a5750a46e84183164cc83add487d34dac
Rampreet21 <rr6975871@gmail.com>
2024-10-25 15:23:54 +0530

    update

--- 13.2 char 2D Array.c
+++ 13.2 char 2D Array.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main()
-{               // Character 2D Array
-    char a[2][9] = {"Rampreet", "Harpreet"};
+{               // single string
+    char b[] = "Rampreetrr";
+    printf("%s\n", b);
 
-    printf("%s %s", a[0], a[1]);
+                // Character 2D Array
+    char a[2][9] = {"Rampreet", "Harpreet"};
+    printf("%s %s\n", a[0], a[1]);
+
+    char c[1][9];
+    scanf("%s", c);
+    printf("%s \n", c[0]);
+
 
     return 0;
 }
